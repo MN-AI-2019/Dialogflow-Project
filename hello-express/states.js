78ef44a9b9085ec3adeb125dbbf069f655afddde
Lihe Chen <lihe.chen@gmail.com>
2019-11-16 07:41:55 -0600

    home work answer. v1

--- hello-express/states.js
+++ hello-express/states.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -23,14 +23,14 @@ app.get('/states', (req, res) => {
 app.get('/states/:code', (req, res) => {
 	console.log(`getting all states for ${req.params.code}`);
 	//validation
-	if (!req.params.code || req.params.code.length != 2) {
+	if (!req.params.code || req.params.code.length !== 2) {
 		res.status(400).send({success: false, error: 'Invalid state code!'})
 	}
 
 	//searching
-	let found = allStates.filter( s => s.code == req.params.code.toUpperCase())
+	let found = allStates.filter( s => s.code === req.params.code.toUpperCase())
 
-	if (found.length == 0) {
+	if (found.length === 0) {
 		res.status(404)
 	}
 
@@ -41,16 +41,14 @@ app.post('/states', function(req, res) {
 	console.log(`adding state ${JSON.stringify(req.body)}`); //need setup body-parser for request.body to work
 	//validation - 400
 	let { code, name } = req.body
-	if (!code || code.length!=2) {
-		res.status(400).send({ success: false, error: 'Invalid state code!' })
+	let validateResult = validateInput(code, name);
+	if (!validateInput.success) {
+		res.status(400).send(validateResult)
 	}
-	if (!name) {
-		res.status(400).send({ success: false, error: 'State name is required!' })
-	}
-	
+
 	//check existing - 409
-	let existing = allStates.filter( s => s.code == code.toUpperCase())
-	if (existing.length != 0) {
+	let existing = allStates.filter( s => s.code === code.toUpperCase())
+	if (existing.length !== 0) {
 		res.status(409).send({ success: false, error: `State ${code} already exists!` })
 	}
 
@@ -61,15 +59,54 @@ app.post('/states', function(req, res) {
 
 app.put('/states/:code', (req, res) => {
 	//validation - 400
+	let code = req.params.code
+	let { name } = req.body
+	let validateResult = validateInput(code, name);
+	if (!validateInput.success) {
+		res.status(400).send(validateResult)
+	}
 	//check exists - 404
-	//remove - 200
-	
+	let existing = allStates.filter( s => s.code === code.toUpperCase())
+	if (existing.length === 0) {
+		res.status(404)
+	}
+	//update - 200
+	allStates.forEach(state => {
+		if (state.code === code.toUpperCase()) {
+			state.name = name
+		}
+	});
+	console.log(JSON.stringify(allStates));
+	res.status(200).send({ success: true, error: `{code} updated` })
 })
 
 app.delete('/states/:code', (req, res) => {
 	//validation - 400
+	let code = req.params.code
+	let { name } = req.body
+	let validateResult = validateInput(code, name);
+	if (!validateInput.success) {
+		res.status(400).send(validateResult)
+	}
 	//check exists - 404
-	//delete - 200
+	let existing = allStates.filter( s => s.code === code.toUpperCase())
+	if (existing.length === 0) {
+		res.status(404)
+	}
+	//remove - 200
+	allStates = allStates.filter( s => s.code !== code.toUpperCase())
+	res.status(200).send({ success: true, error: `{code} removed` })
 })
+
+const validateInput = (code, name) => {
+	
+	if (!code || code.length!=2) {
+		return { success: false, error: 'Invalid state code!' }
+	}
+	if (!name) {
+		return { success: false, error: 'State name is required!' }
+	}
+	return { success: true, error: '' }
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
