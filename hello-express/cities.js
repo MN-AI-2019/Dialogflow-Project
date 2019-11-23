f963e7e1f4ecc9dacddc3bf80e49242092cc46a2
L Chen <jenac@users.noreply.github.com>
2019-11-23 10:34:50 -0600

    Create cities.js

---
+++ hello-express/cities.js
@@ -0,0 +1,8 @@
+
+const cities = [
+    { city: 'edina', state: 'MN', size: 'mid'},
+    { city: 'chicago', state: 'IL', size: 'large'},
+    { city: 'minnetonka', state: 'MN', size: 'mid'},
+    { city: 'madison', state: 'WI', size: 'large'},
+    { city: 'minneapolis', state: 'MN', size: 'large'},
+];
